8ee0fd2ab98a5830801aedb214c6609af7fe4b66
Valdir Lima <valdirlima508@gmail.com>
2022-02-22 12:27:11 -0500

    initial commit

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank_Account balance = new Bank_Account();
+
+            Console.WriteLine("How much would you like to deposit?");
+            double amount = double.Parse(Console.ReadLine());
+
+            balance.Deposit(amount);
+            double userBalance = balance.GetBalance();
+
+            Console.WriteLine($"Your current balance is: {userBalance} ");
+        }
+    }
+}
